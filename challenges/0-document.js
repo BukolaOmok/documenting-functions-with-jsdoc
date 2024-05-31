ebb68085a52e0a2d2ea550ffed81676a4b363bdd
2405-005-BO <2405-005-bo@U-164KH30GEBUC3.corp.amazonworkspaces.com>
2024-05-31 15:27:00 +0100

    add JSDoc comments

--- challenges/0-document.js
+++ challenges/0-document.js
@@ -1,5 +1,12 @@
 // TODO (Ex.0): document and write tests for this function
 
+/** Return the negative version of the number given
+ * 
+ * @param {number} n - the input number
+ * @returns {number} the negative version of `n`
+ */
+
+
 function asNegative(n) {
   // ...
 }
